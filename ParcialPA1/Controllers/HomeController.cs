1ebd44a2aaf7bb73ae5061d22546064d86b9c701
Gersson Leon <ll0204032017@unab.edu>
2021-09-11 17:38:05 -0600

    update

--- ParcialPA1/Controllers/HomeController.cs
+++ ParcialPA1/Controllers/HomeController.cs
@@ -3,24 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ParcialPA1.Models;
 
 namespace ParcialPA1.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "monto")] Monto m)
         {
-            ViewBag.Message = "Your application description page.";
+
+            if (ModelState.IsValid)
+            {
+
+                if (m.monto % 5 == 0) return RedirectToAction("./retiro");
+                return RedirectToAction("./error");
+
+
+
+            }
+
+            return View(m);
+        }
+
+        /*[HttpPost]
+        //public ActionResult Index([Bind(Include ="monto")]Monto monto)
+        public ActionResult Index(int monto, FormCollection collection)
+        {   //usar el operador % para saber el residuo de una division, si este es 0 se puede dividir entre este
+            if (monto % 5==0) return RedirectToAction("./retiro");
+            return HttpNotFound();
+
+        }
+        */
+        public ActionResult retiro()
+        {
+            
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Error()
         {
             ViewBag.Message = "Your contact page.";
 
---
+++ ParcialPA1/Models/Monto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParcialPA1.Models
+{
+    public class Monto
+    {
+        public int monto { get; set; }
+    }
+}
